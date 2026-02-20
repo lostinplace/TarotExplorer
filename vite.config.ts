5b7449da60c5675657944e1496ee3f4a87a7ca2c
Chris Wheeler <cmwhee@gmail.com>
2026-02-20 16:14:34 -0500

    fix: set base path to /TarotExplorer/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import vitePluginYaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
-  base: '/sr-tarot-trainer/',
+  base: '/TarotExplorer/',
   plugins: [
     vitePluginYaml(),
   ],
